5993f0c9c944c3128e809ecd75082e6634d0c75e
Roopz <yiioverflow@gmail.com>
2015-04-03 14:33:27 +0530

    code clean

--- yiitest/public/themes/classic/views/topic/comments/_authorTopics.php
+++ yiitest/public/themes/classic/views/topic/comments/_authorTopics.php
@@ -1,20 +1,32 @@
+<div class="span3" style="margin:0px;">
 <?php
 $this->widget('bootstrap.widgets.TbGridView', array(
     'type' => 'striped bordered condensed',
     'id' => 'topic-grid',
     'afterAjaxUpdate' => 'reinstallDatePickerAndAllDropDown',
+    'hideHeader'=>true,
+    'summaryText'=>false,
+    
     'dataProvider' => $authorTopics,
     'columns' => array(
-        'title',
         array(
-            'htmlOptions' => array('nowrap' => 'nowrap'),
-            'class' => 'bootstrap.widgets.TbButtonColumn',
-            'template' => '{view}'
-        )
+            'name'=>'title',
+            'type'=>'raw',
+            'value' => 'CHtml::link($data->title,Yii::app()->createUrl("topic/view",array("id"=>$data->id)))',
+      ),
     ),
 ));
 ?>
-
-
+</div>
+<style>
+.grid-view table.items tbody tr td:hover
+{
+    background: #CCFF66;
+}
+.grid-view table.items tbody tr td a
+{
+    text-decoration:none;
+}
+</style>
 
 
